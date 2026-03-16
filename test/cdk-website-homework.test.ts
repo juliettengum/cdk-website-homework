a5107fdcf4e9272ff58e984bd6934863692820a0
juliette maloke <your@email.com>
2026-03-16 03:01:26 -0500

    Initial commit

---
+++ test/cdk-website-homework.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkWebsiteHomework from '../lib/cdk-website-homework-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-website-homework-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkWebsiteHomework.CdkWebsiteHomeworkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
